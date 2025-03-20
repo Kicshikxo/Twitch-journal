3daa24c587336fbf716957783f54552dfac83bed
Kicshikxo <supermegavalera9000@gmail.com>
2025-03-21 01:33:44 +0300

    fix(prisma): fix prisma client resolve

--- prisma/nuxt.config.ts
+++ prisma/nuxt.config.ts
@@ -1,1 +1,9 @@
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '.prisma/client/edge': './node_modules/.prisma/client/edge.js',
+      },
+    },
+  },
+})
