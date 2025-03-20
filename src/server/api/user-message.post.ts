71cd26add8c8fae9578885f7edfe17894bf2d990
Kicshikxo <supermegavalera9000@gmail.com>
2025-03-20 21:57:48 +0300

    fix(code): cleanup code

--- src/server/api/user-message.post.ts
+++ src/server/api/user-message.post.ts
@@ -3,15 +3,9 @@ import { AuthRole } from '~/auth/types'
 import { prisma } from '~/prisma/client'
 
 export default defineEventHandler(async (event) => {
-  // const tokenData = readTokenData(event)
-
-  // if (!tokenData || tokenData.role !== AuthRole.CHANNEL_ADMIN) return sendError(event, createError({ statusCode: 401 }))
-  console.log(await readBody(event))
   const { channel, username } = await readBody(event)
 
   if (!channel || !username) return sendError(event, createError({ statusCode: 400 }))
-
-  // if (tokenData.channel !== channel) return sendError(event, createError({ statusCode: 401 }))
 
   const latestStream = await prisma.stream.findFirst({
     where: {
