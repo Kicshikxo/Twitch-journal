78f5994970e962b5fb2e8f369e6d764feb3317ad
Kicshikxo <supermegavalera9000@gmail.com>
2025-03-21 04:56:20 +0300

    feat(refactor): simplify index page

---
+++ src/components/streamer/IndexView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-4 w-full">
+    <StreamerStreamsList v-model="selectedStream" />
+    <StreamerParticipationList :stream="selectedStream" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Stream } from '@prisma/client'
+
+const selectedStream = ref<Stream>()
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="flex flex-1 flex-col items-center gap-4">
-    <div v-if="state.data.value?.role === AuthRole.CHANNEL_STREAMER" class="flex flex-col gap-4 w-full">
-      <StreamerStreamsList v-model="selectedStream" />
-      <StreamerParticipationList :stream="selectedStream" />
-    </div>
-
-    <div v-if="state.data.value?.role === AuthRole.CHANNEL_VIEWER" class="w-full">
-      <ViewerParticipationsList />
-    </div>
+    <StreamerIndexView v-if="state.data.value?.role === AuthRole.CHANNEL_STREAMER" />
+    <ViewerIndexView v-if="state.data.value?.role === AuthRole.CHANNEL_VIEWER" />
   </div>
 </template>
 
 <script setup lang="ts">
-import type { Stream } from '@prisma/client'
 import { AuthRole } from '~/auth/types'
 
 const { state } = useAuth()
-
-const selectedStream = ref<Stream>()
 </script>
