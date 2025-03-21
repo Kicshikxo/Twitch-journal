867ada3440819f86acece06dd14e9ea3b3fe48bb
Kicshikxo <supermegavalera9000@gmail.com>
2025-03-21 17:24:54 +0300

    feat(error): add error page

---
+++ src/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-dvh gap-8">
+    <div class="text-8xl font-bold">
+      {{ error.statusCode }}
+    </div>
+    <div class="text-5xl font-bold mb-4">
+      <span v-if="error.statusCode === 404">Страница не найдена</span>
+      <span v-else-if="error.statusCode === 500">Ошибка сервера</span>
+    </div>
+    <NuxtLink v-if="error.statusCode === 404" to="/">
+      <Button label="На главную">
+        <template #icon>
+          <Icon name="prime:home" />
+        </template>
+      </Button>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ error: any }>()
+</script>
