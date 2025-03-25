f0a2853768f07206bf63225af94b0991f6c5686c
Kicshikxo <supermegavalera9000@gmail.com>
2025-03-26 01:36:57 +0300

    feat(layout): add loading indicator

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col h-dvh">
-    <Toast />
-
+    <NuxtLoadingIndicator color="var(--p-primary-color)" :height="2" :throttle="0" />
     <AppHeader />
     <main class="relative flex flex-1 overflow-y-auto">
+      <Toast />
       <section class="absolute flex p-4 pt-0 w-full min-h-full">
         <slot />
       </section>
